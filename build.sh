f3e4ef4fd924ca6568cd43c6f353793431a26f7c
Ivan Mc Kinley <imckinle@redhat.com>
2016-08-17 18:06:36 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,11 +10,18 @@ if [ ! -e "${DOCKER_SOCKET}" ]; then
 fi
 
 
+if [ -n "$SOURCE_IMAGE}" ]; then
+  SOURCE_TAG="${SOURCE_REGISTRY}/${SOURCE_IMAGE}"
+fi
+
+if [ -n "${TARGET_IMAGE}" ]; then
+  TARGET_TAG="${TARGET_REGISTRY}/${TARGET_IMAGE}"
+fi
 
 docker pull "${SOURCE_TAG}"
 docker tag "${SOURCE_TAG}" ${TARGET_TAG}
 docker push "${TARGET_TAG}" 
-
+ll -r /var/run/secrets/openshift.io/push/
 if [[ -d /var/run/secrets/openshift.io/push ]] && [[ ! -e /root/.dockercfg ]]; then
   cp /var/run/secrets/openshift.io/push/.dockercfg /root/.dockercfg
 fi
