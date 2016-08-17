23cf6f88c95c0fad432dcb7057949d59c5fa53fa
Ivan Mc Kinley <imckinle@redhat.com>
2016-08-17 15:11:02 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,25 +9,6 @@ if [ ! -e "${DOCKER_SOCKET}" ]; then
   exit 1
 fi
 
-if [ -n "${SOURCE_IMAGE}" ]; then
-  SOURCE_TAG="${SOURCE_REGISTRY}/${SOURCE_IMAGE}"
-fi
-
-if [ -n "${TARGET_IMAGE}" ]; then
-  TARGET_TAG="${TARGET_REGISTRY}/${TARGET_IMAGE}"
-fi
-
-if [[ "${SOURCE_REPOSITORY}" != "git://"* ]] && [[ "${SOURCE_REPOSITORY}" != "git@"* ]]; then
-  URL="${SOURCE_REPOSITORY}"
-  if [[ "${URL}" != "http://"* ]] && [[ "${URL}" != "https://"* ]]; then
-    URL="https://${URL}"
-  fi
-  curl --head --silent --fail --location --max-time 16 $URL > /dev/null
-  if [ $? != 0 ]; then
-    echo "Could not access source url: ${SOURCE_REPOSITORY}"
-    exit 1
-  fi
-fi
 
 
 docker pull "${SOURCE_TAG}"
